0427bfb32c55ba771ccbb1a507dec43d995139a1
echarin <wanmingkang@gmail.com>
2023-04-05 10:35:58 +0800

    task 3: two new methods (CR) in userrepo

--- src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
+++ src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
@@ -1,6 +1,41 @@
 package ibf2022.paf.assessment.server.repositories;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import ibf2022.paf.assessment.server.models.User;
+
 // TODO: Task 3
 
+@Repository
 public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate jt;
+
+    public Optional<User> findUserByUsername(String username) {
+        String sql = "select * from user where username = ?";
+        try {
+            User user = jt.queryForObject(sql, BeanPropertyRowMapper.newInstance(User.class), username);
+            return Optional.of(user);
+        } catch (EmptyResultDataAccessException ex) {
+            return Optional.empty();
+        }
+    }
+
+    public String insertUser(User user) {
+        String sql = """
+            insert into user (user_id, username, nickname) values 
+            (?,?,?)
+                """;
+        String uuid = UUID.randomUUID().toString().substring(0, 8);
+        jt.update(sql, uuid, user.getUsername(), user.getName()); // throws DataAccessException
+        return uuid;
+    }
 }
